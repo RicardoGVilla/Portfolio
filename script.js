dbd0bd8c171f574156806ddf1acc48436c56031a
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-06-25 20:43:59 -0400

    adding server script

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-// Arrow scroll down functionality
-const arrow = document.querySelector(".arrow-down");
-arrow.addEventListener("click", function (event) {
-  event.preventDefault();
+// // Arrow scroll down functionality
+// const arrow = document.querySelector(".arrow-down");
+// arrow.addEventListener("click", function (event) {
+//   event.preventDefault();
 
-  const targetSection = document.querySelector(this.getAttribute("href"));
-  targetSection.scrollIntoView({
-    behavior: "smooth",
-  });
+//   const targetSection = document.querySelector(this.getAttribute("href"));
+//   console.log("hi");
+//   targetSection.scrollIntoView({
+//     behavior: "smooth",
+//   });
+// });
+
+///Creating a server side script to receive the form data and send email
+
+console.log("hello world100");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "ricardorgvillanueva@gmail.com",
+    pass: "fuegonegro1229",
+  },
+});
+
+// Check if the transporter was created successfully
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("Transporter creation failed:", error);
+  } else {
+    console.log("Transporter created successfully");
+  }
 });
