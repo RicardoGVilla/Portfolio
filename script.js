70f5aad7f35c33f1c2128dd260235ee5d7a716c8
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-06-27 18:23:18 -0400

    header section finished

--- script.js
+++ script.js
@@ -9,26 +9,3 @@
 //     behavior: "smooth",
 //   });
 // });
-
-///Creating a server side script to receive the form data and send email
-
-console.log("hello world100");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "ricardorgvillanueva@gmail.com",
-    pass: "fuegonegro1229",
-  },
-});
-
-// Check if the transporter was created successfully
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log("Transporter creation failed:", error);
-  } else {
-    console.log("Transporter created successfully");
-  }
-});
--- server.js
+++ server.js
@@ -15,3 +15,37 @@ const port = 3000; // Set the port number you want to use
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.post("/submit-form", (req, res) => {
+  // Extract form data from the request body
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "ricardorgvillanueva@gmail.com",
+      pass: "fuegonegro1228",
+    },
+  });
+
+  // Compose the email message
+  const mailOptions = {
+    from: email,
+    to: "ricardorgvillanueva@gmail.com",
+    subject: "New Form Submission",
+    text: message,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.send("Email sent successfully");
+    }
+  });
+});
