ce40fcdb043e7780bb4334138cc63da589e06ec5
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-08-22 08:44:01 -0400

    testing form

--- server.js
+++ server.js
@@ -1,51 +1,35 @@
-const express = require("express");
 const nodemailer = require("nodemailer");
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-const port = 3000; // Set the port number you want to use
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.post("/submit-form", (req, res) => {
-  // Extract form data from the request body
-  const { name, email, message } = req.body;
+exports.handler = async (event) => {
+  const { name, email, message } = JSON.parse(event.body);
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 465, // Port for secure SSL connection
+    secure: true, // Use SSL
     auth: {
-      user: "ricardorgvillanueva@gmail.com",
-      pass: "fuegonegro1228",
+      user: "ricardorgvillanueva@gmail.com", // Your Gmail email address
+      pass: "Fuegonegro1228.", // Your Gmail password or an "App Password" if enabled
     },
-  });
+  );
 
-  // Compose the email message
   const mailOptions = {
     from: email,
-    to: "ricardorgvillanueva@gmail.com",
+    to: "ricardorgvillanueva@gmail.com", // Replace with your email address
     subject: "New Form Submission",
     text: message,
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log("Error:", error);
-      res.status(500).send("Error sending email");
-    } else {
-      console.log("Email sent:", info.response);
-      res.send("Email sent successfully");
-    }
-  });
-});
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error sending email" }),
+    };
+  }
+};
